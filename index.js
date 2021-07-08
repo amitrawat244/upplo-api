3233906947da74c57ae286ebdf0e826ea43e0070
amit <amit.rawat@techcurl.com>
2021-07-08 12:49:57 +0530

    modified

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-var express = require('express');
+const express = require('express');
+const util = require('util');
 
-var app = express();
+const app = express();
 
 const port = 3300
 
 app.use(express.json());
 
+let headers = {};
 let changeddate = 0;
 app.get('/', (req, res) => {
     console.log("GET");
-    res.send(`File Changed: ${changeddate}`)
+    res.send(`File Changed: ${changeddate}
+        headers:
+        ${util.inspect(headers, { depth: null })}`)
 })
 
 app.get('/clear', (req, res) => {
     console.log("GET");
     changeddate = 0;
+    headers = {};
     res.send(`File Changed: ${changeddate}`)
 })
 
+app.post('/test', (req, res) => {
+    console.log(req.headers);
+    data = req.body;
+    changeddate = Date(Date.now());
+    res.send('ok')
+})
+
 app.post('/notification', (req, res) => {
-    console.log(req.body);
+    console.log(req.headers);
+    headers = req.headers;
     data = req.body;
     changeddate = Date(Date.now());
     res.send('ok')
